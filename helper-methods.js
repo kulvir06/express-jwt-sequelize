a98633da4995d1ebf61a45ab2e510a8b0eb94489
Kulvir Singh <kulvir.singh2019@vitstudent.ac.in>
2021-02-21 23:27:48 +0530

    helper methods added

---
+++ helper-methods.js
@@ -0,0 +1,17 @@
+import db from './database';
+
+class helper{
+    async createUser({ name, password }) {
+        return await db.User.create({ name, password });
+    }
+    
+    async getAllUsers() {
+        return await db.User.findAll();
+    }
+
+    async getUser(obj) {
+        return await User.findOne({ where: obj });
+    }
+}
+
+module.exports = helper;
